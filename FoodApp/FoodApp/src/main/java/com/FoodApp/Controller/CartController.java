82655ee8baccbd3628f2a348786c1632cfc7f4a4
Vedant Pophali <officialvedantpophali2005@gmail.com>
2025-06-06 09:52:15 +0530

    Update CartController.java

--- FoodApp/FoodApp/src/main/java/com/FoodApp/Controller/CartController.java
+++ FoodApp/FoodApp/src/main/java/com/FoodApp/Controller/CartController.java
@@ -4,7 +4,6 @@ import com.FoodApp.IO.CartResponse;
 import com.FoodApp.Service.CartService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
